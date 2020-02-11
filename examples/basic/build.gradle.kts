91d0bf389a43d0f96585012ba7d38f9f61ea8b9c
uzzu <himeatball@gmail.com>
2020-02-12 00:33:52 +0900

    1.0.0 syntax

--- examples/basic/build.gradle.kts
+++ examples/basic/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     base
-    id("co.uzzu.dotenv.gradle") version "1.0-SNAPSHOT"
+    id("co.uzzu.dotenv.gradle") version "1.0.0"
 }
 
 println(env.FOO.orElse("default_foo"))
 println(env.BAR.orNull())
 try {
-    println(env.BAZ.get())
+    println(env.BAZ.value)
 } catch (e: java.lang.IllegalStateException) {
     println("example print: ${e.message}")
 }
-println(env.QUX)
+println(env.QUX.value)
+
