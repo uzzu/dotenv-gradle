5e369969fc28a7b00f600894005c0d85873b8214
uzzu <himeatball@gmail.com>
2022-02-26 18:34:23 +0900

    Move test for basic usage

--- plugin/src/test/kotlin/co/uzzu/dotenv/gradle/BasicUsageTest.kt
+++ plugin/src/test/kotlin/co/uzzu/dotenv/gradle/BasicUsageTest.kt
@@ -2,24 +2,60 @@ package co.uzzu.dotenv.gradle
 
 import assertk.assertThat
 import assertk.assertions.contains
-import assertk.assertions.isNotNull
-import assertk.assertions.messageContains
 import org.gradle.testkit.runner.GradleRunner
-import org.gradle.testkit.runner.UnexpectedBuildFailure
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 
 @Suppress("UnstableApiUsage") // GradleRunner#withPluginClasspath
-class ChangeDotEnvFileTest {
+class BasicUsageTest {
 
     @TempDir
     lateinit var projectDir: File
 
     @Test
-    fun changeFileByEnvironmentVariables() {
+    fun applyToRootProject() {
         RootProject(projectDir) {
             settingsGradle()
+            buildGradle(
+                """
+                plugins {
+                    base
+                    id("co.uzzu.dotenv.gradle")
+                }
+                println("Result: ${'$'}{env.HOGE.value}")
+                """.trimIndent()
+            )
+            file(
+                ".env.template",
+                """
+                HOGE=
+                """.trimIndent()
+            )
+            file(
+                ".env",
+                """
+                HOGE=100
+                """.trimIndent()
+            )
+        }
+
+        val result = GradleRunner.create()
+            .withPluginClasspath()
+            .withProjectDir(projectDir)
+            .build()
+
+        assertThat(result.output).contains("Result: 100")
+    }
+
+    @Test
+    fun applyToSubProject() {
+        RootProject(projectDir) {
+            settingsGradle(
+                """
+                include("sub")
+                """.trimIndent()
+            )
             buildGradle(
                 """
                 plugins {
@@ -47,57 +83,21 @@ class ChangeDotEnvFileTest {
                 HOGE=200
                 """.trimIndent()
             )
+            directory("sub")
+            file(
+                "sub/build.gradle",
+                """
+                println("[Sub] Result: ${'$'}{env.HOGE.value}")
+                """.trimIndent()
+            )
         }
 
         val result = GradleRunner.create()
             .withPluginClasspath()
             .withProjectDir(projectDir)
-            .withEnvironment(mapOf("ENV_FILE" to ".env.staging"))
             .build()
 
-        assertThat(result.output).contains("Result: 200")
-    }
-
-    @Test
-    fun throwsIfChangedFileNotFound() {
-        RootProject(projectDir) {
-            settingsGradle()
-            buildGradle(
-                """
-                plugins {
-                    base
-                    id("co.uzzu.dotenv.gradle")
-                }
-                println("Result: ${'$'}{env.HOGE.value}")
-                """.trimIndent()
-            )
-            file(
-                ".env.template",
-                """
-                HOGE=
-                """.trimIndent()
-            )
-            file(
-                ".env",
-                """
-                HOGE=100
-                """.trimIndent()
-            )
-        }
-
-        val runner = GradleRunner.create()
-            .withPluginClasspath()
-            .withProjectDir(projectDir)
-            .withEnvironment(mapOf("ENV_FILE" to ".env.staging"))
-
-        var error: UnexpectedBuildFailure? = null
-        try {
-            runner.build()
-        } catch (e: UnexpectedBuildFailure) {
-            error = e
-        }
-        assertThat(error)
-            .isNotNull()
-            .messageContains("Could not read the dotenv file specified in the environment variables. ENV_FILE: .env.staging")
+        assertThat(result.output).contains("Result: 100")
+        assertThat(result.output).contains("[Sub] Result: 100")
     }
 }
